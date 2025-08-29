179bb46d5d4723077f4de7ab167ddf052a4863df
Leekhith <146059217+Leekhith@users.noreply.github.com>
2025-08-29 10:37:07 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+  "How to learn Python",
+  "GitHub Pages Tutorial",
+  "AI in Everyday Life",
+  "Understanding Machine Learning",
+  "Building a Portfolio Website",
+  "Best Programming Languages in 2025"
+];
